dd80843b36f833d5534a3020df411da10396320d
Tin Jenda <thanhtin96@gmail.com>
2017-09-30 04:37:49 +0700

    sua loi vat

--- app/src/main/java/com/ute/tinit/chatkotlin/MainActivity.kt
+++ app/src/main/java/com/ute/tinit/chatkotlin/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ute.tinit.chatkotlin
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -10,23 +12,24 @@ import com.ute.tinit.chatkotlin.Fragment.fragment_more
 import com.ute.tinit.chatkotlin.Fragment.fragment_time
 import com.ute.tinit.chatkotlin.Adapter.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity () {
+class MainActivity : AppCompatActivity() {
 
+    var doubleClickExit:Boolean=false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tabViewPage()
     }
 
-    fun tabViewPage()
-    {
-        var viewPageAdapter= ViewPagerAdapter(supportFragmentManager)
-        viewPageAdapter.addFragment(fragment_chat(),"Chat")
-        viewPageAdapter.addFragment(fragment_contacts(),"Contacts")
-        viewPageAdapter.addFragment(fragment_time(),"TimeLine")
-        viewPageAdapter.addFragment(fragment_more(),"More")
-        viewpager_main.adapter=viewPageAdapter
+    fun tabViewPage() {
+        var viewPageAdapter = ViewPagerAdapter(supportFragmentManager)
+        viewPageAdapter.addFragment(fragment_chat(), "Chat")
+        viewPageAdapter.addFragment(fragment_contacts(), "Contacts")
+        viewPageAdapter.addFragment(fragment_time(), "TimeLine")
+        viewPageAdapter.addFragment(fragment_more(), "More")
+        viewpager_main.adapter = viewPageAdapter
 
         viewpager_main.setCurrentItem(0)
         btnChat.isPressed()
@@ -34,30 +37,27 @@ class MainActivity : AppCompatActivity () {
 
         /*Event click change icon tabview*/
         btnChat.setOnClickListener {
-            viewpager_main.setCurrentItem(0,true);
+            viewpager_main.setCurrentItem(0, true);
             clickChat()
         }
-        btnContacts.setOnClickListener{
+        btnContacts.setOnClickListener {
             clickContacts()
-            viewpager_main.setCurrentItem(1,true);
+            viewpager_main.setCurrentItem(1, true);
         }
         btnTimeLine.setOnClickListener {
             clickTimeline()
-            viewpager_main.setCurrentItem(2,true);
+            viewpager_main.setCurrentItem(2, true);
         }
         btnMore.setOnClickListener {
             clickMore()
-            viewpager_main.setCurrentItem(3,true);
-
+            viewpager_main.setCurrentItem(3, true);
         }
 
         viewpager_main.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {
-
             }
 
             override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-
             }
 
             override fun onPageSelected(position: Int) {
@@ -70,65 +70,88 @@ class MainActivity : AppCompatActivity () {
                         btnContacts.isPressed()
                         clickContacts()
                     }
-                    2->{
+                    2 -> {
                         btnTimeLine.isPressed()
                         clickTimeline()
                     }
-                    3->{
+                    3 -> {
                         btnMore.isPressed()
                         clickMore()
                     }
                 }
             }
-
-
-
         })
-   }
+    }
 
-    fun clickChat()
-    {
+    fun clickChat() {
         btnChat.setImageResource(R.drawable.ic_message_active)
         btnMore.setImageResource(R.drawable.ic_more_black)
         btnTimeLine.setImageResource(R.drawable.ic_timeline_black)
         btnContacts.setImageResource(R.drawable.ic_contacts_black)
-        btnInsertMore.visibility= View.VISIBLE
-        btnInsertFriend.visibility=View.GONE
-        btnInsertPicture.visibility=View.GONE
-        btnSetting.visibility=View.GONE
+        btnInsertMore.visibility = View.VISIBLE
+        btnInsertFriend.visibility = View.GONE
+        btnInsertPicture.visibility = View.GONE
+        btnSetting.visibility = View.GONE
     }
-    fun clickContacts()
-    {
+
+    fun clickContacts() {
         btnContacts.setImageResource(R.drawable.ic_contact_active)
         btnMore.setImageResource(R.drawable.ic_more_black)
         btnTimeLine.setImageResource(R.drawable.ic_timeline_black)
         btnChat.setImageResource(R.drawable.ic_message_black)
-        btnInsertMore.visibility= View.GONE
-        btnInsertFriend.visibility=View.VISIBLE
-        btnInsertPicture.visibility=View.GONE
-        btnSetting.visibility=View.GONE
+        btnInsertMore.visibility = View.GONE
+        btnInsertFriend.visibility = View.VISIBLE
+        btnInsertPicture.visibility = View.GONE
+        btnSetting.visibility = View.GONE
     }
-    fun clickTimeline()
-    {
-        btnInsertMore.visibility= View.GONE
-        btnInsertFriend.visibility=View.GONE
-        btnInsertPicture.visibility=View.VISIBLE
-        btnSetting.visibility=View.GONE
+
+    fun clickTimeline() {
+        btnInsertMore.visibility = View.GONE
+        btnInsertFriend.visibility = View.GONE
+        btnInsertPicture.visibility = View.VISIBLE
+        btnSetting.visibility = View.GONE
         btnTimeLine.setImageResource(R.drawable.ic_timeline_active)
         btnMore.setImageResource(R.drawable.ic_more_black)
         btnContacts.setImageResource(R.drawable.ic_contacts_black)
         btnChat.setImageResource(R.drawable.ic_message_black)
     }
-    fun clickMore()
-    {
-        btnInsertMore.visibility= View.GONE
-        btnInsertFriend.visibility=View.GONE
-        btnInsertPicture.visibility=View.GONE
-        btnSetting.visibility=View.VISIBLE
+
+    fun clickMore() {
+        btnInsertMore.visibility = View.GONE
+        btnInsertFriend.visibility = View.GONE
+        btnInsertPicture.visibility = View.GONE
+        btnSetting.visibility = View.VISIBLE
         btnMore.setImageResource(R.drawable.ic_more_active)
         btnTimeLine.setImageResource(R.drawable.ic_timeline_black)
         btnContacts.setImageResource(R.drawable.ic_contacts_black)
         btnChat.setImageResource(R.drawable.ic_message_black)
+    }
+
+    //back to return current item 1 => exit
+    override fun onBackPressed() {
+        if (doubleClickExit) {
+            super.onBackPressed()
+            return
+        }
+
+        if(viewpager_main.currentItem==3||viewpager_main.currentItem==2||viewpager_main.currentItem==1)
+        {
+            viewpager_main.currentItem=0
+            return
+        }
+        this@MainActivity.doubleClickExit = true
+        Handler().postDelayed(Runnable { doubleClickExit = false }, 1000)
+    }
+
+
+    fun toastShow(text: String,context:Context) { /*show toast*/
+        var m_currentToast: Toast? = null
+        if (m_currentToast != null) {
+            m_currentToast!!.cancel()
+        }
+        m_currentToast = Toast.makeText(context, text, Toast.LENGTH_SHORT)
+        m_currentToast!!.show()
+
     }
 
 }
