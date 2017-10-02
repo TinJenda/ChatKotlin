2d44ee7d4906a96e1e0c9a01e9a52bbd9885dacb
Tin Jenda <thanhtin96@gmail.com>
2017-10-03 01:00:33 +0700

    profile update 1

---
+++ app/src/main/java/com/ute/tinit/chatkotlin/Activity/activity_profile_more_myprofile_changeprofile.kt
@@ -0,0 +1,12 @@
+package com.ute.tinit.chatkotlin.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_profile_more_myprofile_changeprofile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_more_myprofile_changeprofile)
+    }
+}
