7306fa43e0847ec9fd9d42d27f75475581b42b62
Tin Jenda <thanhtin96@gmail.com>
2017-10-17 20:34:57 +0700

    bug... :(

--- app/src/main/java/com/ute/tinit/chatkotlin/Activity/activity_profile_more_myprofile.kt
+++ app/src/main/java/com/ute/tinit/chatkotlin/Activity/activity_profile_more_myprofile.kt
@@ -3,6 +3,7 @@ package com.ute.tinit.chatkotlin.Activity
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.Dialog
 import android.app.ProgressDialog
 import android.content.ContentResolver
 import android.content.Intent
@@ -15,21 +16,21 @@ import com.ute.tinit.chatkotlin.R
 import kotlinx.android.synthetic.main.layout_activity_profile_more_myprofile.*
 import android.net.Uri
 import android.os.Build
+import android.provider.MediaStore
 import android.util.Log
-import android.webkit.MimeTypeMap
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
 import android.widget.Toast
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.storage.OnProgressListener
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.UploadTask
-import com.mvc.imagepicker.ImagePicker
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.Target
-import com.theartofdev.edmodo.cropper.CropImage
 import com.ute.tinit.chatkotlin.Adapter.BlurImage
 import io.vrinda.kotlinpermissions.PermissionCallBack
 import io.vrinda.kotlinpermissions.PermissionsActivity
-import kotlinx.android.synthetic.main.layout_activity_find_friend_location.*
 import java.io.ByteArrayOutputStream
 import java.io.FileNotFoundException
 import java.io.IOException
@@ -37,9 +38,8 @@ import java.io.IOException
 class activity_profile_more_myprofile : PermissionsActivity() {
 
     private val BLUR_PRECENTAGE = 50
-    private val IMAGE_URL = "https://firebasestorage.googleapis.com/v0/b/chatkotlin-tinjenda.appspot.com/o/avarta%2Favarta.jpg?alt=media&token=84d76d2f-6d6c-4929-b8db-63e8c00d35f7"
-    private var DATA_UPDATE:ByteArray?=null
-    private var mCropImageUri:Uri?=null
+    private var IMAGE_URL = "https://firebasestorage.googleapis.com/v0/b/chatkotlin-tinjenda.appspot.com/o/avarta%2F1508246074211.JPEG?alt=media&token=24d2a77e-471f-482a-b375-c621eafd5809"
+    private var DATA_UPDATE: ByteArray? = null
     private var mStorageRef: StorageReference? = null
     var imgUri: Uri? = null
 
@@ -58,6 +58,7 @@ class activity_profile_more_myprofile : PermissionsActivity() {
         blurImage()
         mStorageRef = FirebaseStorage.getInstance().getReference();
         btnDoiAnhDaiDien()
+        logoAnimation()
         doiThongTin()
     }
 
@@ -86,7 +87,7 @@ class activity_profile_more_myprofile : PermissionsActivity() {
             }
 
             override fun onBitmapFailed(errorDrawable: Drawable) {
-                toolbarImage1.setImageResource(R.mipmap.ic_launcher)
+                toolbarImage1.setImageResource(R.drawable.null_image)
             }
 
             override fun onPrepareLoad(placeHolderDrawable: Drawable) {
@@ -95,22 +96,23 @@ class activity_profile_more_myprofile : PermissionsActivity() {
         toolbarImage1.setTag(target)
         Picasso.with(this)
                 .load(IMAGE_URL)
-                .error(R.mipmap.ic_launcher)
-                .placeholder(R.mipmap.ic_launcher)
+                .error(R.drawable.null_image)
+                .resize(800, 800)
+                .placeholder(R.drawable.null_image)
                 .into(target)
     }
 
 
     fun doiThongTin() {
         btn_doithongtin.setOnClickListener {
-            btnUpload()
+            btnUploadCamera()
         }
     }
 
     //test upload image to storage firebase and get link that image
     fun btnDoiAnhDaiDien() {
 
-        myprofile_anh_dai_dien.setOnClickListener {
+        cap_nhap_anh_dd.setOnClickListener {
             requestPermissions(Manifest.permission.CAMERA, object : PermissionCallBack {
                 @SuppressLint("MissingPermission")
                 override fun permissionGranted() {
@@ -133,127 +135,196 @@ class activity_profile_more_myprofile : PermissionsActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (data != null) {
-            Log.d("AAA","REQUESE_CODE = "+ REQUEST_CODE)
-            Log.d("AAA","requestCode = "+ requestCode)
-            Log.d("AAA","RESULT_OK = "+ RESULT_OK)
-            Log.d("AAA","resultCode = "+resultCode)
-            if(requestCode== REQUEST_CODE&&resultCode== RESULT_OK&&data.data!=null)
-            {
-                imgUri=data.data
-                try {
-//                    var bm:Bitmap=MediaStore.Images.Media.getBitmap(contentResolver,imgUri)
-//                    myprofile_anh_dai_dien.setImageBitmap(bm)
+            Log.d("AAA", "REQUESE_CODE = " + REQUEST_CODE)
+            Log.d("AAA", "requestCode = " + requestCode)
+            Log.d("AAA", "RESULT_OK = " + RESULT_OK)
+            Log.d("AAA", "resultCode = " + resultCode)
+            if (requestCode == 1 && resultCode == RESULT_OK) {
 
-                     var bitmap:Bitmap  = ImagePicker.getImageFromResult(this@activity_profile_more_myprofile,
-                             requestCode, resultCode, data)!!
+                try {
+                    var bitmap: Bitmap = data.getExtras().get("data") as Bitmap;
+                    myprofile_anh_dai_dien.setImageBitmap(bitmap);
+                    //
                     val baos = ByteArrayOutputStream()
                     //giam dung luong truoc khi day len firebase :(
-                    bitmap.compress(Bitmap.CompressFormat.JPEG,25,baos)
-                    DATA_UPDATE = baos.toByteArray()
-                    Toast.makeText(this@activity_profile_more_myprofile,"Vao update", Toast.LENGTH_SHORT).show()
-                    myprofile_anh_dai_dien.setImageBitmap(bitmap)
-                }
-                catch (e:FileNotFoundException)
-                {
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos)
+                    DATA_UPDATE=baos.toByteArray()
+                    Toast.makeText(this@activity_profile_more_myprofile, "Vao update", Toast.LENGTH_SHORT).show()
+                } catch (e: FileNotFoundException) {
+                    e.printStackTrace()
+                } catch (e: IOException) {
                     e.printStackTrace()
                 }
-                catch (e:IOException)
-                {
-                    e.printStackTrace()
-                }
-                  // handle result of pick image chooser
 
-//                  if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-//                    var imageUri:Uri  = CropImage.getPickImageResultUri(this@activity_profile_more_myprofile, data);
-//
-//                    // For API >= 23 we need to check specifically that we have permissions to read external storage.
-//                    if (CropImage.isReadExternalStoragePermissionsRequired(this, imageUri)) {
-//                      // request permissions and handle the result in onRequestPermissionsResult()
-//                      mCropImageUri = imageUri
-//                        var listPermission= arrayOf("Manifest.permission.READ_EXTERNAL_STORAGE")
-//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                            requestPermissions(listPermission,   CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE)
-//                        };
-//                    } else {
-//                      // no permissions required or already grunted, can start crop image activity
-//                      startCropImageActivity(imageUri);
-//                    }
-//                  }
+            } else {
+                if (requestCode == 2 && resultCode == RESULT_OK) {
+                    try {
+                        val selectedImage = data!!.getData()
+                        myprofile_anh_dai_dien.setImageURI(selectedImage)
+                        val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImage)
+                        myprofile_anh_dai_dien.setImageBitmap(bitmap);
+                        val baos = ByteArrayOutputStream()
+                        //giam dung luong truoc khi day len firebase :(
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+                        DATA_UPDATE = baos.toByteArray()
+                        imgUri=selectedImage
+                        Toast.makeText(this@activity_profile_more_myprofile, "Vao update", Toast.LENGTH_SHORT).show()
+                    }
+                    catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                }
             }
-        }
-        else{
-            Log.d("AAA","DATA NULL")
+        } else {
+            Log.d("AAA", "DATA NULL")
+
         }
     }
     //
 
-    fun  showFileChooser() {
-        onPickImage()
-}
-    fun getImageExt(uri:Uri): String? {
-        var contentResolver:ContentResolver=contentResolver
-        var mimeTypeMap:MimeTypeMap= MimeTypeMap.getSingleton()
-        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri))
-    }
-    fun btnUpload()
-    {
+    fun showFileChooser() {
+        val dialog = Dialog(this@activity_profile_more_myprofile)
+        // Include dialog.xml file
+        dialog.setContentView(R.layout.dialog_list_select_image)
+        // Set dialog title
+        dialog.setTitle("")
 
-        if(imgUri!=null)
-        {
+        // set values for custom dialog components - text, image and button
+        val btnCamera = dialog.findViewById<Button>(R.id.btnCamera)
+        val btnThuVien = dialog.findViewById<Button>(R.id.btnThuVien)
+
+        btnCamera.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, 1)
+            dialog.dismiss()
+        }
+
+        btnThuVien.setOnClickListener {
+            requestPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, object : PermissionCallBack {
+                @SuppressLint("MissingPermission")
+                override fun permissionGranted() {
+                    super.permissionGranted()
+
+                    val pickPhoto = Intent(Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    startActivityForResult(pickPhoto, 2)
+
+                }
+
+                override fun permissionDenied() {
+                    super.permissionDenied()
+                    Log.v("Call permissions", "Denied")
+                    Toast.makeText(this@activity_profile_more_myprofile, "Vui lòng bật/chấp nhận quyền bộ nhớ để thực hiện tính năng này", Toast.LENGTH_SHORT).show()
+
+                }
+            })
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    fun btnUploadGallery() {
+
+        if (DATA_UPDATE != null) {
             var dialog = ProgressDialog(this@activity_profile_more_myprofile)
             dialog.setTitle("Uploading image")
             dialog.show()
             //resize
             //get storage ref
-            var ref:StorageReference= mStorageRef!!.child(FB_STORAGE_PATH+System.currentTimeMillis()+"."+getImageExt(imgUri!!))
-
+            var ref: StorageReference = mStorageRef!!.child(FB_STORAGE_PATH + System.currentTimeMillis() + "." +"png")
             ref.putBytes(DATA_UPDATE!!)
-                    .addOnSuccessListener(object :OnSuccessListener<UploadTask.TaskSnapshot>{
+                    .addOnSuccessListener(object : OnSuccessListener<UploadTask.TaskSnapshot> {
                         override fun onSuccess(p0: UploadTask.TaskSnapshot?) {
                             dialog.dismiss()
-                            var imgUploadLink:String= p0!!.getDownloadUrl().toString()
+                            var imgUploadLink: String = p0!!.getDownloadUrl().toString()
                             Toast.makeText(this@activity_profile_more_myprofile,
-                                    "Image Uploaded -> "+imgUploadLink ,Toast.LENGTH_SHORT).show()
-                            Log.d("BBB",imgUploadLink)
+                                    "Image Uploaded -> " + imgUploadLink, Toast.LENGTH_SHORT).show()
+                            Log.d("BBB", imgUploadLink)
                         }
                     })
                     .addOnFailureListener {
                         dialog.dismiss()
                         Toast.makeText(this@activity_profile_more_myprofile,
-                                "Image Error",Toast.LENGTH_SHORT).show()
+                                "Image Error", Toast.LENGTH_SHORT).show()
 
                     }
                     .addOnProgressListener(object : OnProgressListener<UploadTask.TaskSnapshot> {
                         override fun onProgress(taskSnapshot: UploadTask.TaskSnapshot?) {
-                            var progress: Long =(100* taskSnapshot!!.bytesTransferred)/taskSnapshot.totalByteCount
-                            dialog.setMessage("Uploading "+progress.toInt()+"%")
+                            var progress: Long = (100 * taskSnapshot!!.bytesTransferred) / taskSnapshot.totalByteCount
+                            dialog.setMessage("Uploading " + progress.toInt() + "%")
                         }
 
                     })
-        }
-        else
-        {
+        } else {
             Toast.makeText(this@activity_profile_more_myprofile,
-                    "Select Image Error",Toast.LENGTH_SHORT).show()
+                    "Select Image Error", Toast.LENGTH_SHORT).show()
         }
     }
+    fun btnUploadCamera() {
 
-      fun onPickImage(  ) {
-        // Click on image button
-        ImagePicker.pickImage(this, "Select your image:");
+        if (DATA_UPDATE != null) {
+            var dialog = ProgressDialog(this@activity_profile_more_myprofile)
+            dialog.setTitle("Uploading image")
+            dialog.show()
+            //resize
+            var ref: StorageReference = mStorageRef!!.child(FB_STORAGE_PATH + System.currentTimeMillis() + "." + "JPEG")
+
+            ref.putBytes(DATA_UPDATE!!)
+                    .addOnSuccessListener(object : OnSuccessListener<UploadTask.TaskSnapshot> {
+                        override fun onSuccess(p0: UploadTask.TaskSnapshot?) {
+                            dialog.dismiss()
+                            var imgUploadLink: String = p0!!.getDownloadUrl().toString()
+                            Toast.makeText(this@activity_profile_more_myprofile,
+                                    "Image Uploaded -> " + imgUploadLink, Toast.LENGTH_SHORT).show()
+                            //set image test
+                            IMAGE_URL=imgUploadLink
+                            val target = object : Target {
+                                override fun onBitmapLoaded(bitmap: Bitmap, from: Picasso.LoadedFrom) {
+                                    toolbarImage1.setImageBitmap(BlurImage.fastblur(bitmap, BLUR_PRECENTAGE))
+                                }
+
+                                override fun onBitmapFailed(errorDrawable: Drawable) {
+                                    toolbarImage1.setImageResource(R.drawable.null_image)
+                                }
+
+                                override fun onPrepareLoad(placeHolderDrawable: Drawable) {
+                                }
+                            }
+                            toolbarImage1.setTag(target)
+                            Picasso.with(this@activity_profile_more_myprofile)
+                                    .load(IMAGE_URL)
+                                    .error(R.drawable.null_image)
+                                    .placeholder(R.drawable.null_image)
+                                    .centerCrop()
+                                    .resize(800, 800)
+                                    .into(target)
+                            //set image test
+                            Log.d("BBB", imgUploadLink)
+                        }
+                    })
+                    .addOnFailureListener {
+                        dialog.dismiss()
+                        Toast.makeText(this@activity_profile_more_myprofile,
+                                "Image Error", Toast.LENGTH_SHORT).show()
+
+                    }
+                    .addOnProgressListener(object : OnProgressListener<UploadTask.TaskSnapshot> {
+                        override fun onProgress(taskSnapshot: UploadTask.TaskSnapshot?) {
+                            var progress: Long = (100 * taskSnapshot!!.bytesTransferred) / taskSnapshot.totalByteCount
+                            dialog.setMessage("Uploading " + progress.toInt() + "%")
+                        }
+
+                    })
+        } else {
+            Toast.makeText(this@activity_profile_more_myprofile,
+                    "Select Image Error", Toast.LENGTH_SHORT).show()
+        }
     }
-
-    private fun startCropImageActivity() {
-        CropImage.activity()
-    .start(this@activity_profile_more_myprofile);
-    }
-
-    fun onSelectImageClick() {
-        CropImage.startPickImageActivity(this@activity_profile_more_myprofile)
-    }
-
-    private fun startCropImageActivity(imageUri: Uri) {
-        CropImage.activity(imageUri)
-                .start(this@activity_profile_more_myprofile)
+    fun logoAnimation() {
+        var animation: Animation = AnimationUtils.loadAnimation(this@activity_profile_more_myprofile, R.anim.ani_camera)
+        cap_nhap_anh_dd.startAnimation(animation)
     }
 }
